4f209b674858c7bdb6b616b8fc953a65acf349ca
KatiaBasmaci <kb3133@columbia.edu>
2020-05-13 19:32:43 +0000

    App urls config

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Sightings
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import Count
 
-# Create your views here.
+from django.http import HttpResponse
+
+def sightings(request):
+    all_sightings = Sightings.objects.all()
+    context = {
+            'all_sightings': all_sightings,
+    }
+    return render(request,'sightings.html',context)
