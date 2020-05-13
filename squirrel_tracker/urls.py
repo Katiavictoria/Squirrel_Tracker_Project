c851a3b43469a46411e84ab02828595bc3478036
ms5962 <ms5962@columbia.edu>
2020-05-13 20:28:41 +0000

    Add map view

--- squirrel_tracker/urls.py
+++ squirrel_tracker/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sightings/', views.sightings),]
+    path('sightings/', views.sightings),
+    path('map/',views.map),
+    ]
--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -11,3 +11,10 @@ def sightings(request):
             'all_sightings': all_sightings,
     }
     return render(request,'sightings.html',context)
+
+def map(request):
+    all_sightings = Sightings.objects.all()[:100]
+    context = {
+            'all_sightings': all_sightings,
+    }
+    return render(request,'map.html',context)
